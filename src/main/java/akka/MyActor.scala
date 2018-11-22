99764fb20c7d083a5c6b8bc469fc642c047b67fe
caiweiwei <caiweiwei@jd.com>
2018-11-22 18:01:50 +0800

    akka

---
+++ src/main/java/akka/MyActor.scala
@@ -0,0 +1,14 @@
+package akka
+
+import akka.actor.Actor
+import akka.event.Logging
+
+class MyActor extends Actor{
+
+  val log = Logging(context.system,this)
+
+  override def receive = {
+    case "test" => log.info("receive test")
+    case _ => log.info("receive other message")
+  }
+}
