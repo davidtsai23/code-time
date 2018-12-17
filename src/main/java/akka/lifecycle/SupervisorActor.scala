00664ad79c365872a65a8d09669a6d7f5abc8a1f
caiweiwei <caiweiwei@jd.com>
2018-12-17 17:29:59 +0800

    akka actor lifecycle

---
+++ src/main/java/akka/lifecycle/SupervisorActor.scala
@@ -0,0 +1,35 @@
+package akka.lifecycle
+
+import akka.actor.{Actor, Props}
+
+object SupervisorActor{
+  def props:Props = Props(new SupervisorActor)
+}
+
+class SupervisorActor extends Actor{
+
+  val child = context.actorOf(SupervisedActor.props,"child")
+  override def receive: Receive = {
+    case "failChild" ⇒ child ! "fail"
+  }
+
+}
+
+object SupervisedActor {
+  def props: Props =
+    Props(new SupervisedActor)
+}
+
+
+class SupervisedActor extends Actor{
+
+
+  override def preStart(): Unit = println("supervised actor started")
+  override def postStop(): Unit = println("supervised actor stopped")
+
+  override def receive: Receive = {
+    case "fail" =>
+      println("supervised actor fails now")
+      throw new Exception("I failed!")
+  }
+}
