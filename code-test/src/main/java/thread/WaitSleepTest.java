8e2b1e04b755c598aec20f8e93d3ed59c21ef50b
caiweiwei <caiweiwei@jd.com>
2019-03-06 11:29:04 +0800

    wait sleep

--- code-test/src/main/java/thread/WaitSleepTest.java
+++ code-test/src/main/java/thread/WaitSleepTest.java
@@ -21,7 +21,7 @@ public class WaitSleepTest {
             synchronized (object){
                 System.out.println("thread:"+Thread.currentThread().getName()+"enter,Time:"+System.currentTimeMillis());
                 try {
-                    object.wait();//Object 方法
+                    object.wait(5000);//Object 方法，线程释放当前对象锁进入等待对象锁定池进行等待
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -43,8 +43,8 @@ public class WaitSleepTest {
 
                 try {
                     //object.wait(2000);
-                    Thread.sleep(2000);
-                    object.notify();
+                    Thread.sleep(2000); //不会释放当前对象锁，直到走完2S执行完synchronized代码块
+                    //object.notify(); //如果注释掉这一句，那么Thread-0exit将不会输出，针对wait()方法，而wait(5000)没有notify也会继续执行
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
