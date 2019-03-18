8b456af9b34bd96572749d01e7cf5026afe0a575
caiweiwei <caiweiwei@jd.com>
2019-03-18 17:31:10 +0800

    countDownLatch

--- code-test/src/main/java/com/jfs/jvm/StringTest.java
+++ code-test/src/main/java/com/jfs/jvm/StringTest.java
@@ -13,7 +13,8 @@ public class StringTest {
         System.out.println(s == s2);
 
         String s3 = new String("1") + new String("1");
-        //intern 这个方法是一个 native 的方法，但注释写的非常明了。“如果常量池中存在当前字符串, 就会直接返回当前字符串. 如果常量池中没有此字符串, 会将此字符串放入常量池中后, 再返回”。
+        //intern 这个方法是一个 native 的方法，但注释写的非常明了。
+        // “如果常量池中存在当前字符串, 就会直接返回当前字符串. 如果常量池中没有此字符串, 会将此字符串放入常量池中后, 再返回”。
         System.out.println(s3.intern());
         String s4 = "11";
         System.out.println(s3 == s4);
--- code-web/src/test/java/redis/RedisLockTest.java
+++ code-web/src/test/java/redis/RedisLockTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,16 +19,48 @@ public class RedisLockTest {
 
     @Test
     public void testRedisLock(){
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        CountDownLatch latch = new CountDownLatch(1);
+        for (int i=0;i<countDownLatch.getCount();i++){
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        latch.await(); //每个线程在此等待
+                        System.out.println("==================开始竞争锁资源================"+Thread.currentThread().getName());
+                        RLock lock = redissonClient.getLock("lockKey");
+                        boolean res = lock.tryLock(3, 10, TimeUnit.SECONDS);
+                        if (res) {
+                            System.out.println("==================竞争锁资源成功================"+Thread.currentThread().getName());
+                            Thread.sleep(1000);//模拟执行1S
+                            lock.unlock();
+                        } else {
+                            System.out.println("==================竞争锁资源失败================"+Thread.currentThread().getName());
+                        }
+                        countDownLatch.countDown();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
-        testReentrantLock();
-        testReentrantLock2();
-        Thread thread = new Thread(new Thread1());
-        thread.start();
+                }
+            });
+            thread.start();
+        }
+
+        try {
+            System.out.println("==================3秒后开始竞争锁资源==================");
+            Thread.sleep(3000);
+            latch.countDown(); // 触发所有线程全部开始执行
+            countDownLatch.await(); // 等待线程全部执行完毕
+            System.out.println("==================结束==================");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
 
     }
 
-
+    @Test
     public void testReentrantLock(){
         RLock lock = redissonClient.getLock("anyLock");
         try{
@@ -36,10 +69,12 @@ public class RedisLockTest {
             // 2. 支持过期解锁功能,10秒钟以后自动解锁, 无需调用unlock方法手动解锁
             //lock.lock(10, TimeUnit.SECONDS);
             // 3. 尝试加锁，最多等待3秒，上锁以后10秒自动解锁
-            boolean res = lock.tryLock(3, 100, TimeUnit.SECONDS);
+            boolean res = lock.tryLock(3, 10, TimeUnit.SECONDS);
             if(res){ //成功
                 // do your business
-                dealBusiness(lock.getName());
+                dealBusiness(Thread.currentThread().getName());
+            }else {
+
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -48,30 +83,27 @@ public class RedisLockTest {
         }
     }
 
-    public void testReentrantLock2(){
-        RLock lock = redissonClient.getLock("anyLock");
-        try{
-            // 1. 最常见的使用方法
-            //lock.lock();
-            // 2. 支持过期解锁功能,10秒钟以后自动解锁, 无需调用unlock方法手动解锁
-            lock.lock(100, TimeUnit.SECONDS);
+    class BusinessThread implements Runnable{
 
-            dealBusiness(lock.getName());
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            //lock.unlock();
+        CountDownLatch countDownLatch;
+        public BusinessThread(CountDownLatch countDownLatch){
+            this.countDownLatch = countDownLatch;
         }
-    }
-
-    class Thread1 implements Runnable{
-
         @Override
         public void run() {
             try {
-                dealBusiness("thread");
-            } catch (InterruptedException e) {
+                System.out.println("==================开始竞争锁资源================"+Thread.currentThread().getName());
+                RLock lock = redissonClient.getLock("lockKey");
+                boolean res = lock.tryLock(3, 10, TimeUnit.SECONDS);
+                if (res) {
+                    System.out.println("==================竞争锁资源成功================"+Thread.currentThread().getName());
+                    Thread.sleep(5000);//模拟执行5S
+                    lock.unlock();
+                } else {
+                    System.out.println("==================竞争锁资源失败================"+Thread.currentThread().getName());
+                }
+                countDownLatch.countDown();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
