b7032634d97f00124c543949b304ad4fa0973e1f
caiweiwei <caiweiwei@jd.com>
2019-03-26 11:24:24 +0800

    动态代理

---
+++ code-test/src/main/java/com/jfs/jvm/proxy/dynamic/RentersDynamicProxy.java
@@ -0,0 +1,26 @@
+package com.jfs.jvm.proxy.dynamic;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class RentersDynamicProxy implements InvocationHandler {
+    private Object target;
+
+    public RentersDynamicProxy(Object target) {
+        this.target = target;
+    }
+
+    // 为目标对象生成代理对象
+    public Object getProxyInstance(){
+        return Proxy.newProxyInstance(target.getClass().getClassLoader(),target.getClass().getInterfaces(),this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("开始动态代理");
+        method.invoke(target,args);
+        System.out.println("动态代理结束");
+        return null;
+    }
+}
--- code-test/src/main/java/com/jfs/jvm/proxy/statics/TestProxy.java
+++ code-test/src/main/java/com/jfs/jvm/proxy/statics/TestProxy.java
@@ -1,7 +1,22 @@
 package com.jfs.jvm.proxy.statics;
 
+import com.jfs.jvm.proxy.dynamic.RentersDynamicProxy;
+
 public class TestProxy {
     public static void main(String[] args) {
+
+        testDynamicProxy();
+    }
+
+    public static void testDynamicProxy(){
+        RentersService target = new RentersServiceImpl();
+        RentersService rentersService = (RentersService) new RentersDynamicProxy(target).getProxyInstance();
+        System.out.println(target.getClass());
+        System.out.println(rentersService.getClass());
+        rentersService.rentHouse();
+    }
+
+    public static void testStaticProxy(){
         //目标对象
         RentersService rentersService = new RentersServiceImpl();
         //代理对象
