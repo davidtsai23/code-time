a182fa930492209fa5f13589599e559b9dce71a0
caiweiwei02 <caiweiwei02@meituan.com>
2019-05-26 23:02:59 +0800

    completableFuture

--- code-test/src/main/java/com/jfs/thread/CompletableFutureTest.java
+++ code-test/src/main/java/com/jfs/thread/CompletableFutureTest.java
@@ -1,9 +1,6 @@
 package com.jfs.thread;
 
-import com.google.common.collect.Lists;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 import java.util.concurrent.*;
 
 /**
@@ -13,58 +10,171 @@ import java.util.concurrent.*;
 public class CompletableFutureTest {
 
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        List<String> stringList = new ArrayList<>();
-        List<String> testList = Lists.newArrayList();
-        testList.add("1");
-        testList.add("2");
-        long start = System.currentTimeMillis();
-        CompletableFuture[] futures = testList.stream().
-                map(t -> CompletableFuture
-                        .supplyAsync(() ->  {
-                            /*if (true) {
-                                throw new RuntimeException("Something wrong");
-                            }*/
-                            return "1";
-                        }, executor)
-                        .whenComplete((v,e)->{
-                            stringList.add(v);
-                        })
-                        ).toArray(CompletableFuture[]::new);
-
-        CompletableFuture.allOf(futures);
-        //completableFuture.get(3000, TimeUnit.MILLISECONDS);
-        System.out.println("block finish, consume time:" + (System.currentTimeMillis() - start));
-
-        executor.shutdown();
-
-
+        ExecutorService executor = Executors.newFixedThreadPool(20);
+//        thenApply();
+//        thenCombine();
+//        exception();
+//        whenComplete();
+//        handle();
+        allof();
     }
 
-    public static void test(){
-        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("future1 finished!");
-            return "future1 finished!";
-        });
-        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
-            System.out.println("future2 finished!");
-            return "future2 finished!";
-        });
-        CompletableFuture<Void> combindFuture = CompletableFuture.allOf(future1, future2);
+    private static String mockRpc(String s) {
         try {
-            combindFuture.get();
+            if ("1".equals(s)){
+
+                Thread.sleep(1000);
+            }
+            if ("2".equals(s)){
+                Thread.sleep(2000);
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
         }
-        System.out.println("future1: " + future1.isDone() + " future2: " + future2.isDone());
+        return s;
     }
 
+    public static void thenApply(){
+        CompletableFuture<String> futureA = CompletableFuture.supplyAsync(() -> "任务A");
+        CompletableFuture<String> futureB = CompletableFuture.supplyAsync(() -> "任务B");
+        CompletableFuture<String> futureC = futureB.thenApply(b -> {
+            System.out.println("执行任务C.");
+            System.out.println("参数:" + b);//参数:任务B
+            return "a";
+        });
+    }
+
+    /**
+     * 需要根据商品id查询商品的当前价格,分两步,查询商品的原始价格和折扣,这两个查询相互独立,当都查出来的时候用原始价格乘折扣,算出当前价格.
+     */
+    public static void thenCombine(){
+        CompletableFuture<Double> futurePrice = CompletableFuture.supplyAsync(() -> 100d);
+        CompletableFuture<Double> futureDiscount = CompletableFuture.supplyAsync(() -> 0.8);
+        CompletableFuture<Double> futureResult = futurePrice.thenCombine(futureDiscount, (price, discount) -> price * discount);
+        System.out.println("最终价格为:" + futureResult.join()); //最终价格为:80.0
+    }
+
+
+    public static void exception(){
+        CompletableFuture<String> futureA = CompletableFuture.
+                supplyAsync(() -> "执行结果:" + (100 / 0))
+                .thenApply(s -> "futureA result:" + s)
+                .exceptionally(e -> {
+                    System.out.println(e.getMessage()); //java.lang.ArithmeticException: / by zero
+                    return "futureA result: 100";
+                });
+        CompletableFuture<String> futureB = CompletableFuture.
+                supplyAsync(() -> "执行结果:" + 50)
+                .thenApply(s -> "futureB result:" + s)
+                .exceptionally(e -> "futureB result: 100");
+        System.out.println(futureA.join());//futureA result: 100
+        System.out.println(futureB.join());//futureB result:执行结果:50
+    }
+
+
+    public static void whenComplete(){
+        CompletableFuture<String> futureA = CompletableFuture.
+                supplyAsync(() -> "执行结果:" + (100 / 0))
+                .thenApply(s -> "apply result:" + s)
+                .whenComplete((s, e) -> {
+                    if (s != null) {
+                        System.out.println(s);//未执行
+                    }
+                    if (e == null) {
+                        System.out.println(s);//未执行
+                    } else {
+                        System.out.println(e.getMessage());//java.lang.ArithmeticException: / by zero
+                    }
+                })
+                .exceptionally(e -> {
+                    System.out.println("ex"+e.getMessage()); //ex:java.lang.ArithmeticException: / by zero
+                    return "futureA result: 100"; });
+        System.out.println(futureA.join());//futureA result: 100
+
+
+        CompletableFuture<String> futureB = CompletableFuture.
+                supplyAsync(() -> "执行结果:" + (100 / 0))
+                .thenApply(s -> "apply result:" + s)
+                .exceptionally(e -> {
+                    System.out.println("ex:"+e.getMessage()); //ex:java.lang.ArithmeticException: / by zero
+                    return "futureB result: 100";
+                })
+                .whenComplete((s, e) -> {
+                    if (e == null) {
+                        System.out.println(s);//futureA result: 100
+                    } else {
+                        System.out.println(e.getMessage());//未执行
+                    }
+                })
+                ;
+        System.out.println(futureB.join());//futureA result: 100
+    }
+
+    public static void handle(){
+        CompletableFuture<String> futureA = CompletableFuture.
+                supplyAsync(() -> "执行结果:" + (100 / 0))
+                .thenApply(s -> "apply result:" + s)
+                .exceptionally(e -> {
+                    System.out.println("ex:" + e.getMessage()); //java.lang.ArithmeticException: / by zero
+                    return "futureA result: 100";
+                })
+                .handle((s, e) -> {
+                    if (e == null) {
+                        System.out.println(s);//futureA result: 100
+                    } else {
+                        System.out.println(e.getMessage());//未执行
+                    }
+                    return "handle result:" + (s == null ? "500" : s);
+                });
+        System.out.println(futureA.join());//handle result:futureA result: 100
+    }
+
+    public static void allof(){
+
+            ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+            long start = System.currentTimeMillis();
+            CompletableFuture<String> futureA = CompletableFuture.supplyAsync(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return "商品详情";
+            },executorService);
+
+            CompletableFuture<String> futureB = CompletableFuture.supplyAsync(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return "卖家信息";
+            },executorService);
+
+            CompletableFuture<String> futureC = CompletableFuture.supplyAsync(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return "库存信息";
+            },executorService);
+
+            CompletableFuture<String> futureD = CompletableFuture.supplyAsync(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return "订单信息";
+            },executorService);
+
+            CompletableFuture<Void> allFuture = CompletableFuture.allOf(futureA, futureB, futureC, futureD);
+            allFuture.join();
+
+            System.out.println(futureA.join() + futureB.join() + futureC.join() + futureD.join());
+            System.out.println("总耗时:" + (System.currentTimeMillis() - start));
+    }
 
 }
